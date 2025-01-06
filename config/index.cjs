e2b6617464a7316f84efe07a07a5462447bbc802
Rai <56507074+lincong1498@users.noreply.github.com>
2025-01-06 22:18:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,9 +15,6 @@ const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
 USE_PASSAGE: 'push-plus',
 
-  PROVINCE: '吉林',
-  CITY: '长春',
-
   USERS: [
     {
       // 想要发送的人的名字
@@ -25,22 +22,7 @@ USE_PASSAGE: 'push-plus',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '33e5fab4b9504516986a30f0aca1f2f6',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '08-09',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1992', date: '08-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
+      useTemplateId: '0003',
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
@@ -53,7 +35,7 @@ USE_PASSAGE: 'push-plus',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
